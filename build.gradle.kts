e5fc3cb9eab71e682de528ba2fb20ac9f20ab4f8
idonuntius <mc.natsu831@gmail.com>
2022-02-01 23:48:30 +0900

    Convert build.gradle and settings.gradle to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.4"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"
-        classpath 'com.google.dagger:hilt-android-gradle-plugin:2.28-alpha'
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.28-alpha")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
+        maven { setUrl("https://jitpack.io") }
     }
 }
 rootProject.name = "AndroidStarter"
-include ':app'
+include(":app")
