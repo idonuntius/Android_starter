74d01d6018158c8845f2471c25c2e002e13b82a7
idonuntius <mc.natsu831@gmail.com>
2022-02-01 23:49:41 +0900

    Add App and move MainActivity

---
+++ app/src/main/java/com/idonuntius/androidstarter/App.kt
@@ -0,0 +1,7 @@
+package com.idonuntius.androidstarter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
--- app/src/main/java/com/idonuntius/androidstarter/presentation/view/MainActivity.kt
+++ app/src/main/java/com/idonuntius/androidstarter/presentation/view/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.idonuntius.androidstarter
+package com.idonuntius.androidstarter.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.idonuntius.androidstarter.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
